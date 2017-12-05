7dd456c114afef10b8f597567338e350f0325384
Ian Egner <ian.egner@apc-overnight.com>
2017-12-05 09:11:22 +0000

    Test coverage

--- index.js
+++ index.js
@@ -1,8 +1,68 @@
 const BaseModule = require('requireg')('huestatus/src/Module')
+const Sentry = require('sentry-api').Client
 
 class HueTry extends BaseModule {
-  async start () {
+  /**
+   * Generate instance name based on project and organisation
+   * @return {String} [description]
+   */
+  generateInstanceName () {
+    return `Sentry: ${this.config.organisation}/${this.config.project}`
+  }
 
+  /**
+   * Start method, called on huestatus start. Loops through api polling
+   */
+  async start () {
+    this._setUpConfig()
+    setInterval(this._pollSentry.bind(this), this.config.pollInterval || 2000)
+  }
+
+  /**
+   * Check for config variables and create Sentry API Client
+   * @throws error when required config value is not set
+   */
+  _setUpConfig () {
+    ['sentryApiKey', 'organisation', 'project'].map(configItem => {
+      if (!this.config[configItem]) {
+        throw new Error(`Sentry ${configItem} config value not set`)
+      }
+    })
+
+    this.sentry = new Sentry({ token: this.config.sentryApiKey })
+  }
+
+  /**
+   * Make a request to the sentry API, check for unresolved issues, then filter out already assigned issues
+   * @return {Promise}
+   */
+  async _pollSentry () {
+    const issues = await this.sentry.get(`projects/${this.config.organisation}/${this.config.project}/issues/`)
+
+    const unassignedIssues = issues.filter(issue => !issue.assignedTo)
+
+    if (unassignedIssues.length) {
+      return this._alert(unassignedIssues.length)
+    }
+
+    return this._ok()
+  }
+
+  /**
+   * Set the status to ok
+   * @return {Promise}
+   */
+  async _ok () {
+    await this.change('ok', 'No unassigned unresolved issues')
+  }
+
+  /**
+   * Set the status to alert and log the number of issues
+   * @param  {Number}  issuesCount Number of issues
+   * @return {Promise}
+   */
+  async _alert (issuesCount) {
+    await this.change('alert', `${issuesCount} unassigned unresolved issues`)
   }
 }
 
---
+++ test/huetry.spec.js
@@ -0,0 +1,149 @@
+/* eslint-disable no-unused-expressions */
+
+const chai = require('chai')
+chai.use(require('sinon-chai'))
+const expect = chai.expect
+const sinon = require('sinon')
+const HueTry = require('../')
+
+let mockConfig
+let huetry
+let sentryStub
+let changeStub
+
+describe('HueTry Class', function () {
+  beforeEach(() => {
+    mockConfig = {
+      name: 'huetry',
+      sentryApiKey: '3abxxxxxxxxxxxxxxxxx0f5',
+      project: 'myProject',
+      organisation: 'myOrg',
+      pollInterval: 5000,
+      light: 'Hue color lamp 2'
+    }
+
+    huetry = new HueTry(mockConfig, {})
+  })
+
+  it('It should set up the sentry api client', async () => {
+    expect(huetry.config).to.be.an('object')
+    expect(huetry.sentry).to.be.undefined
+
+    huetry._setUpConfig()
+
+    expect(huetry.sentry).to.be.an('object')
+  })
+
+  it('It should set status to ok if there are no issues', async () => {
+    const huetry = new HueTry(mockConfig, {})
+    huetry._setUpConfig()
+    sentryStub = sinon.stub(huetry.sentry, 'get').resolves([])
+    changeStub = sinon.stub(huetry, 'change').resolves(true)
+
+    await huetry._pollSentry()
+
+    expect(sentryStub).to.be.calledWith('projects/myOrg/myProject/issues/')
+    expect(changeStub).to.be.calledWith('ok', 'No unassigned unresolved issues')
+
+    sentryStub.restore()
+    changeStub.restore()
+  })
+
+  it('It should set status to ok if there are no unassigned issues', async () => {
+    const huetry = new HueTry(mockConfig, {})
+    huetry._setUpConfig()
+    sentryStub = sinon.stub(huetry.sentry, 'get').resolves([{ assignedTo: 'Ian' }])
+    changeStub = sinon.stub(huetry, 'change').resolves(true)
+
+    await huetry._pollSentry()
+
+    expect(sentryStub).to.be.calledWith('projects/myOrg/myProject/issues/')
+    expect(changeStub).to.be.calledWith('ok', 'No unassigned unresolved issues')
+
+    sentryStub.restore()
+    changeStub.restore()
+  })
+
+  it('It should set status to alert if there are unassigned issues', async () => {
+    const huetry = new HueTry(mockConfig, {})
+    huetry._setUpConfig()
+    sentryStub = sinon.stub(huetry.sentry, 'get').resolves([{ assignedTo: null }, { assignedTo: null }])
+    changeStub = sinon.stub(huetry, 'change').resolves(true)
+
+    await huetry._pollSentry()
+
+    expect(sentryStub).to.be.calledWith('projects/myOrg/myProject/issues/')
+    expect(changeStub).to.be.calledWith('alert', '2 unassigned unresolved issues')
+
+    sentryStub.restore()
+    changeStub.restore()
+  })
+
+  it('Should throw when config variable is missing', () => {
+    mockConfig.organisation = undefined
+    const huetry = new HueTry(mockConfig, {})
+    expect(() => { huetry._setUpConfig() }).to.throw('Sentry organisation config value not set')
+  })
+
+  it('Should generate instance name based on org and project', () => {
+    const huetry = new HueTry(mockConfig, {})
+    expect(huetry.instanceName).to.equal('Sentry: myOrg/myProject')
+  })
+
+  it('Should poll every x seconds', async () => {
+    this.clock = sinon.useFakeTimers()
+    const huetry = new HueTry(mockConfig, {})
+    huetry._setUpConfig()
+
+    sinon.stub(huetry, '_pollSentry')
+
+    await huetry.start()
+
+    this.clock.tick(3000)
+
+    expect(huetry._pollSentry).to.not.be.called
+
+    this.clock.tick(3000)
+
+    expect(huetry._pollSentry).to.be.calledOnce
+
+    this.clock.tick(3000)
+
+    expect(huetry._pollSentry).to.be.calledOnce
+
+    this.clock.tick(3000)
+
+    expect(huetry._pollSentry).to.be.calledTwice
+
+    this.clock.restore()
+  })
+
+  it('Poll every 2 seconds by default', async () => {
+    this.clock = sinon.useFakeTimers()
+    mockConfig.pollInterval = undefined
+    const huetry = new HueTry(mockConfig, {})
+    huetry._setUpConfig()
+
+    sinon.stub(huetry, '_pollSentry')
+
+    await huetry.start()
+
+    this.clock.tick(1200)
+
+    expect(huetry._pollSentry).to.not.be.called
+
+    this.clock.tick(1200)
+
+    expect(huetry._pollSentry).to.be.calledOnce
+
+    this.clock.tick(1200)
+
+    expect(huetry._pollSentry).to.be.calledOnce
+
+    this.clock.tick(1200)
+
+    expect(huetry._pollSentry).to.be.calledTwice
+
+    this.clock.restore()
+  })
+})
